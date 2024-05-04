3b12b50f9ae7e84ed5ead46adb3c3ee3ce4cd46a
ArtyomHloba <hlobartyom@gmail.com>
2024-05-04 18:22:45 +0300

    Add basis of JS

---
+++ assets/js/index.js
@@ -0,0 +1,63 @@
+// alert('Hello1');
+
+// let, const - змінні
+// var - старий стиль
+// debugger 
+// let age = 18;
+// alert(age);
+// age = 19;
+// alert(age);
+
+// const count = 10;
+// alert(count);
+
+// const PI = 3.14;
+
+// number 0 1 -10 159, 15.88, -0, Infinity, -Infinity, NaN -  not a number
+
+// let n1 = 99999999999999999999999;
+// alert(n1);
+
+// bigint
+
+// let bigNumber = 99999999999999999999999n;
+// alert(bigNumber);
+
+// string
+
+// let userName = 'Artyom';
+// alert(userName);
+
+// let phrase = "His name is 'Ivo'";
+// alert(phrase);
+
+// let someHTML = `
+//     <h1>head</h1>
+//     <p>pharagrah</p>  
+// `;
+
+// document.write(someHTML);
+
+// boolean - true, false
+// let  isEmail = true;
+// alert(isEmail);
+
+// стать
+// let gender = "male";
+// стать
+// let isMale = false; // true -ч, false - ж
+
+// let name = "Artem";
+// let email = "artem@gmail.com";
+// let ageHuman = "19";
+// let gender = "male";
+// alert("Name:" + name + "\nEmail:" + email);
+// console.log("Name:", name, "\nEmail:", email);
+
+// null
+// let a = null;
+
+// undefined
+// let b;
+
+// не примітив - object
